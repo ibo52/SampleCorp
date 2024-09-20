7e294d406a326355486ca008c12c1b0226785c35
ibrahim mut <halilibrahimmut@hotmaiil.com>
2024-09-20 16:58:21 +0300

    change Home View design backbone

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,18 +2,18 @@
 import Title from '@/components/Title.vue';
 </script>
 <template>
-    <Title
-    title="Welcome"
-    description="to the fairy shop of tailorship."/>
-    <div class="flex flex-col gap-4 p-8">
+    <div class="grid grid-cols-12 w-full lg:h-[39rem] relative">
+        <div class="col-span-5 bg-white">
 
-        <h2 class="text-4xl font-bold">Some Paragraph header</h2>
-        <p class="flex text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar quam et dolor fringilla elementum. Vestibulum accumsan bibendum ante et bibendum. Aliquam et cursus metus. Aliquam erat volutpat. Suspendisse aliquam tellus diam, ac rutrum mi volutpat in. Phasellus vehicula justo felis, vel elementum urna euismod at. Etiam sit amet dolor nibh. Nulla id odio id tellus mattis porta tempor at sem.
+            <Title class="relative bg-transparent my-8 left-16 pr-0"
+            title="Your work is unique."
+            description="Because we cutting and fitting the fabrics"/>
 
-Duis id massa sit amet tellus dapibus posuere. Vestibulum sed eros eget elit rhoncus pellentesque ac placerat turpis. Suspendisse facilisis, leo et luctus aliquet, ipsum arcu malesuada nibh, in placerat ex lacus sit amet ante. Nullam eu consequat nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in enim ultricies, suscipit sem sit amet, sagittis arcu. Ut purus lectus, tincidunt a dui eget, porta eleifend enim. Donec rhoncus id diam ut porta. Curabitur nec velit vitae ligula molestie scelerisque quis ullamcorper mauris. Aenean ultrices lorem tincidunt ex imperdiet malesuada. Suspendisse tempus est non odio efficitur, eu lobortis eros luctus. Proin porttitor aliquam aliquam. In condimentum lacus at turpis pretium, nec ullamcorper lectus auctor. Nullam porttitor varius tellus, id ultricies leo tincidunt ut. Nunc a tempor dui. Pellentesque ut finibus dolor, a tempor risus.
+        </div>
 
-Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus suscipit viverra lacus, sed aliquam orci interdum non. Morbi accumsan urna id massa suscipit, sed pellentesque enim cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Integer tempor massa euismod, tincidunt nisi at, rutrum justo. Nam erat ex, consectetur quis nisi at, luctus lacinia tellus. Integer tristique porttitor orci ac auctor. Mauris euismod eu elit quis elementum. Maecenas quis euismod nisl. Etiam aliquet bibendum ornare. Curabitur at mi at risus faucibus gravida. Fusce sed convallis mi. Integer leo magna, placerat in nisi sit amet, lacinia sollicitudin quam. Aliquam interdum at ante a iaculis. Morbi mauris velit, interdum quis auctor ac, venenatis at neque.
-        </p>
+        <!-- right split-->
+         <div class="col-span-7 bg-cover bg-center bg-[url('../resources/tailoring.jpg')]">
+            <div class="bg-pink-700/80 relative h-full w-full"></div>
+         </div>
     </div>
 </template>

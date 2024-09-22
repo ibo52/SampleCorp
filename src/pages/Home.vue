2d932e3810d19a0e3a5e7e1f2b1bfced13e9e894
ibrahim mut <halilibrahimmut@hotmaiil.com>
2024-09-22 19:13:30 +0300

    change home layout, add bg image and Title at top

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,18 +2,60 @@
 import Title from '@/components/Title.vue';
 </script>
 <template>
-    <div class="grid grid-cols-12 w-full lg:h-[39rem] relative">
-        <div class="col-span-5 bg-white">
 
-            <Title class="relative bg-transparent my-8 left-16 pr-0"
-            title="Your work is unique."
-            description="Because we cutting and fitting the fabrics"/>
+    <div class="w-full items-center">
 
+        <div class="flex flex-col
+        lg:grid lg:grid-cols-12
+        bg-cover bg-center bg-[url('../resources/tailoring.jpg')] bg-fixed h-[29rem] relative"
+        >
+                    <!--title positioned as absolute-->
+                    <Title class="relative lg:absolute mx-5 my-20 lg:m-20 rounded-md lg:p-2 border-cyan-900 border-[12px] overflow-scroll
+                    bg-black/20 z-10 text-white mix-blend-color-dodge"
+                                title="Your work is unique."
+                                description="Because we cutting and fitting the fabrics."/>
+
+                    <!-- left split when enlarged-->
+                    <!-- flex box on small screens-->
+                    <div class="absolute lg:static flex lg:col-span-5 bg-black/80 h-full w-full mix-blend-overlay">
+                    </div>
+
+                    <!-- right split when enlarged-->
+                    <div class="hidden lg:flex lg:col-span-7
+                    bg-pink-800/90 mix-blend-overlay">
+                    </div>
         </div>
+        <div class="mx-8 my-8 gap-8">
 
-        <!-- right split-->
-         <div class="col-span-7 bg-cover bg-center bg-[url('../resources/tailoring.jpg')]">
-            <div class="bg-pink-700/80 relative h-full w-full"></div>
-         </div>
-    </div>
+            <Title
+            class="bg-transparent"
+            title="Let us introduce Ourselves"/>
+        <p>
+            We’re all about making good, lovely custom clothing to
+        be worn on most memorable days. We’re passionate about
+        changing the way you look at clothing. We want to share
+        our years of experience of cutting and sewing to suit your appearance.
+        It’s time to start wearing clothes that fashionable,
+        share your enthusiasm of trends with us.
+        We’re all about making good, lovely custom clothing to
+        be worn on most memorable days. We’re passionate about
+        changing the way you look at clothing. We want to share
+        our years of experience of cutting and sewing to suit your appearance.
+        It’s time to start wearing clothes that fashionable,
+        share your enthusiasm of trends with us.
+        We’re all about making good, lovely custom clothing to
+        be worn on most memorable days. We’re passionate about
+        changing the way you look at clothing. We want to share
+        our years of experience of cutting and sewing to suit your appearance.
+        It’s time to start wearing clothes that fashionable,
+        share your enthusiasm of trends with us.
+        We’re all about making good, lovely custom clothing to
+        be worn on most memorable days. We’re passionate about
+        changing the way you look at clothing. We want to share
+        our years of experience of cutting and sewing to suit your appearance.
+        It’s time to start wearing clothes that fashionable,
+        share your enthusiasm of trends with us.
+        </p>
+        </div>
+</div>
 </template>

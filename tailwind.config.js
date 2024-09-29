7da984350dd4ba7d953d2846d63fec808564bdeb
ibrahim mut <halilibrahimmut@hotmaiil.com>
2024-09-29 13:15:28 +0300

    add gradient color animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,43 @@ export default {
         'satisfy':['Satisfy', 'cursive']
       }
     },
+    'animation': {
+            'gradient-x':'gradient-x 12s ease infinite',
+            'gradient-y':'gradient-y 12s ease infinite',
+            'gradient-xy':'gradient-xy 12s ease infinite',
+        },
+        'keyframes': {
+            'gradient-y': {
+                '0%, 100%': {
+                    'background-size':'400% 400%',
+                    'background-position': 'center top'
+                },
+                '50%': {
+                    'background-size':'200% 200%',
+                    'background-position': 'center center'
+                }
+            },
+            'gradient-x': {
+                '0%, 100%': {
+                    'background-size':'200% 200%',
+                    'background-position': 'left center'
+                },
+                '50%': {
+                    'background-size':'200% 200%',
+                    'background-position': 'right center'
+                }
+            },
+            'gradient-xy': {
+                '0%, 100%': {
+                    'background-size':'400% 400%',
+                    'background-position': 'left center'
+                },
+                '50%': {
+                    'background-size':'200% 200%',
+                    'background-position': 'right center'
+                }
+            }
+        }
   },
   plugins: [],
 }

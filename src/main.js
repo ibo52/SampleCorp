4ee3f8930d4b6ee1c8f596044870610b6dc1e516
ibrahim mut <halilibrahimmut@hotmaiil.com>
2024-10-10 13:40:54 +0300

    update router pages

--- src/main.js
+++ src/main.js
@@ -7,17 +7,23 @@ import App from './pages/App.vue'
 import Home from './pages/Home.vue'
 import About from './pages/About.vue'
 import ContactUs from './pages/ContactUs.vue'
+import Test from './pages/Test.vue'
+import Portfolio from './pages/Portfolio.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
     { path: '/contact', component: ContactUs },
-    { path: '/reserved', component: Home }
+    { path: '/portfolio', component: Portfolio },
+    { path: '/test', component: Test }
   ]
 
-  const router = createRouter({
+const router = createRouter({
     history: createMemoryHistory(),
     routes,
-  })
+    scrollBehavior (to, from, savedPosition) {
+        return { top:0 }
+    }
+})
 
 createApp(App).use(router).mount('#app')
